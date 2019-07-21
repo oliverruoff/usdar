a92dfd13c4285dd472b0086e6766ed420abd32dd
Oliver Ruoff <oliver93ruoff@web.de>
2019-07-21 21:10:36 +0200

    added debug logs

--- Usdar.py
+++ Usdar.py
@@ -10,13 +10,15 @@ def get_coord(angle, distance):
 def scan_360():
     env_map = scan_range(360, True)
     # move stepper back to initial position
-    st.turn_stepper_angle(180, False)
+    st.turn_stepper_angle(360, False)
     return env_map
 
 def scan_range(degree, clockwise=True):
     env_map = []
     for angle in range(degree):
+        print('Angle:', angle)
         distance = us.get_distance()
+        print('Distance:', distance)
         env_map.append(get_coord(angle, distance))
         st.turn_stepper_angle(1, clockwise)
     return env_map
