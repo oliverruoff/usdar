7a6aa06fe99fa360d2f128e238fd5d696f03d728
Oliver Ruoff <oliver93ruoff@web.de>
2019-07-22 21:34:36 +0200

    changed to simple distance function

--- Usdar.py
+++ Usdar.py
@@ -4,6 +4,7 @@ import math
 import Stepper as st
 import UltraSonic as us
 
+
 def get_coord(angle, distance):
     #print('>>>sin(', angle, '=', math.sin(angle), '*', distance, '=',math.sin(angle)*distance )
     #print('>>>cos(', angle, '=', math.cos(angle), '*', distance, '=',math.cos(angle)*distance )
@@ -13,15 +14,15 @@ def get_coord(angle, distance):
 def scan_360():
     env_map = []
     stepper_turn_multiplier = 2
-    for i in range (256):
+    for i in range(256):
         angle = 360/512*i*stepper_turn_multiplier
-        distance = us.get_reliable_distance()
-        #print('____________________')
+        distance = us.get_distance()
+        # print('____________________')
         #print('Angle   :', angle)
         #print('Distance:', distance)
         #print('X       :', get_coord(angle, distance)[0])
         #print('Y       :', get_coord(angle, distance)[1])
-        #print('____________________')
+        # print('____________________')
         env_map.append(get_coord(math.radians(angle), distance))
         st.run_stepper(stepper_turn_multiplier, True)
     # Reset stepper position
