2e8dfa951acd42183c897c84fea961fdb39edf43
Oliver Ruoff <oliver93ruoff@web.de>
2019-08-03 19:25:13 +0200

    added median distance

--- UltraSonic.py
+++ UltraSonic.py
@@ -15,13 +15,20 @@ GPIO.setup(GPIO_ECHO, GPIO.IN)
 
 
 def get_reliable_distance(number_combined_measurements=5):
-    measurements = [get_distance() for dist in range(number_combined_measurements)]
+    measurements = [get_distance()
+                    for dist in range(number_combined_measurements)]
     avg_distance = sum(measurements) / len(measurements)
     avg_deviations = [abs(avg_distance - dist) for dist in measurements]
     avg_deviation = sum(avg_deviations) / len(avg_deviations)
-    filtered_distances = [measurements[i] for i in range(len(avg_deviations)) if avg_deviations[i] < avg_deviation ]
+    filtered_distances = [measurements[i] for i in range(
+        len(avg_deviations)) if avg_deviations[i] < avg_deviation]
     improved_avg_distance = sum(filtered_distances) / len(filtered_distances)
     return improved_avg_distance
+
+
+def get_median_distance(number_combined_measurements=5):
+    return median([get_distance() for dist
+                   in range(number_combined_measurements)])
 
 
 def get_distance():
