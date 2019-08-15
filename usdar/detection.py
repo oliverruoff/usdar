ae16aa176af5fb809523653d6c1915b88bad4d8f
Oliver Ruoff <oliver93ruoff@web.de>
2019-08-15 21:48:29 +0200

    fix direction function

--- usdar/detection.py
+++ usdar/detection.py
@@ -18,10 +18,10 @@ def scan_360(stepper_turn_multiplier=2, direction=True):
     env_map = []
     for i in range(int(512/stepper_turn_multiplier)):
         if not direction:
-            i = mc.move_back() - i
+            i = int(512/stepper_turn_multiplier) - i
         angle = 360/512*i*stepper_turn_multiplier
         distance = get_distance()
         radians_angle = math.radians(angle)
         env_map.append(get_coord(radians_angle, distance))
-        run_stepper(stepper_turn_multiplier, direction)
+        run_stepper(stepper_turn_multiplier, True)
     return env_map
